404af7c386992934dab4f4564d2a13678c8b51dd
ataur65 <ataur.rahman6846@gmail.com>
2025-09-24 01:31:59 +0600

    Fix: Import useState in BlogPosts component

--- components/BlogPosts.tsx
+++ components/BlogPosts.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { BlogPost } from '@/lib/interfaces';
